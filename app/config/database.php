56870c36f54c2cda59ff41722e118603e553c1df
Meibisu <meibisu42@gmail.com>
2023-03-20 14:58:01 +0300

    add config/database.php

---
+++ app/config/database.php
@@ -0,0 +1,15 @@
+<?php
+
+class database
+{
+    private string $host = "localhost";
+    private string $db_name = "task_db";
+    private string $username = "test";
+    private string $password = "toor";
+    public string $conn;
+
+    public function getConnection(){
+        // connection
+    }
+
+}
